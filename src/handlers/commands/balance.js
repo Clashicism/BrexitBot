ec7acb444e3ddb81b68269177c136d39a670afa6
Clashicism <60586534+Clashicism@users.noreply.github.com>
2021-06-17 18:53:48 -0400

    Update balance.js

--- src/handlers/commands/balance.js
+++ src/handlers/commands/balance.js
@@ -10,7 +10,7 @@ module.exports.balance = async ctx => {
   const honkPoints = session.wallet.honkPoints;
 
   ctx.replyWithMarkdown(
-    `*${ctx.from.first_name}* your balance: *${honkPoints.toLocaleString('en-US')}* ⚛️ Atom ⚛️`,
+    `*${ctx.from.first_name}* your balance: *${honkPoints.toLocaleString('en-US')}* ⚛️ Salient ⚛️`,
     Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
       .oneTime()
       .resize()
