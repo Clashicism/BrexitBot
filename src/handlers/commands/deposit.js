a21efacea712b43df9589474828b746e1e6633fb
Clashicism <60586534+Clashicism@users.noreply.github.com>
2021-06-17 18:54:35 -0400

    Update deposit.js

--- src/handlers/commands/deposit.js
+++ src/handlers/commands/deposit.js
@@ -6,7 +6,7 @@ module.exports.deposit = async ctx => {
   const SLPaddress = ctx.session.wallet.SLPaddress;
 
   ctx.replyWithMarkdown(
-    `⚛️ Atomic HOW TO DEPOSIT ⚛️\n${ctx.from.first_name} deposit tokens to this address:\n\n \`\`\`${SLPaddress}\`\`\``,
+    `⚛️ Salient HOW TO DEPOSIT ⚛️\n${ctx.from.first_name} deposit tokens to this address:\n\n \`\`\`${SLPaddress}\`\`\``,
     Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
       .oneTime()
       .resize()
